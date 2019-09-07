eed10c09fb5323b25b73010f00465d8581881945
Rayan Osseiran <rayano24@Hotmail.com>
2019-09-07 11:51:09 -0400

    Initial Commit

---
+++ CardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by Rayan Osseiran on 2019-09-07.
+//  Copyright © 2019 RayLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
